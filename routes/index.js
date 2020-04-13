0cfa872e0717fa501f904e4c00225520aa8d6f7c
Sergey Karlinskiy <s9219871110@gmail.com>
2020-04-13 22:20:51 +0300

    added 2 new routes for signin and signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
-
+const { auth } = require('../middlewares');
 const usersRouter = require('./users');
 const articlesRouter = require('./articles');
 const errorRouter = require('./error');
+const signInRouter = require('./signin');
+const signUpRouter = require('./signup');
 
+router.use('/signup', signUpRouter);
+router.use('/signin', signInRouter);
 
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/articles', articlesRouter);
 router.use('/', errorRouter);
