a80c8621f0f7e3e5790acd2f581f51132e4c0e20
Sergey Karlinskiy <s9219871110@gmail.com>
2020-04-15 16:33:19 +0300

    deleted stringValidator

--- middlewares/celebrateValidators.js
+++ middlewares/celebrateValidators.js
@@ -1,33 +1,30 @@
 const { celebrate, Joi } = require('celebrate');
 
-const stringValidator = /([А-ЯЁ][а-яё]+-?)+/;
 const urlValidator = /http(s?):\/\/(www\.)?((\w|[a-яё]|-)+((\.(\w|[a-яё]|-)+){1,4})?\.(\w|[a-яё]|-)+)(:(\d{2,5}))?(\w|\/|\\)+#?/;
 const passValidator = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/;
 
-const Errors = require('../errors/Errors');
-const {
-  BAD_ID, BAD_EMAIL, BAD_NAME, BAD_PASS, SHORT_PASS, BAD_URL,
-} = require('../errors/messages');
+const error = require('../helpers');
+const message = require('../constants');
 
 const signIn = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email().error(new Errors.BadRequest(BAD_EMAIL)),
-    password: Joi.string().required().min(6).error(new Errors.BadRequest(SHORT_PASS)),
+    email: Joi.string().required().email().error(new error.BadRequest(message.BAD_EMAIL)),
+    password: Joi.string().required().min(6).error(new error.BadRequest(message.SHORT_PASS)),
   }),
 });
 
 const signUp = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email()
-      .error(new Errors.BadRequest(BAD_EMAIL)),
+      .error(new error.BadRequest(message.BAD_EMAIL)),
     password: Joi.string()
       .pattern(new RegExp(passValidator))
       .required()
       .min(6)
-      .error(new Errors.BadRequest(BAD_PASS)),
-    name: Joi.string().pattern(new RegExp(stringValidator)).required().min(2)
+      .error(new error.BadRequest(message.BAD_PASS)),
+    name: Joi.string().required().min(2)
       .max(30)
-      .error(new Errors.BadRequest(BAD_NAME)),
+      .error(new error.BadRequest(message.BAD_NAME)),
   }),
 });
 
@@ -41,18 +38,18 @@ const createValid = celebrate({
     link: Joi.string()
       .pattern(new RegExp(urlValidator))
       .required()
-      .error(new Errors.BadRequest(BAD_URL)),
+      .error(new error.BadRequest(message.BAD_URL)),
     image: Joi.string()
       .pattern(new RegExp(urlValidator))
       .required()
-      .error(new Errors.BadRequest(BAD_URL)),
+      .error(new error.BadRequest(message.BAD_URL)),
   }),
 });
 
 const idValid = celebrate({
   params: Joi.object().keys({
     id: Joi.string().alphanum().length(24)
-      .error(new Errors.BadRequest(BAD_ID)),
+      .error(new error.BadRequest(message.BAD_ID)),
   }),
 });
 
