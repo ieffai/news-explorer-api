151026243bd5ce779b39d93d76da9dd5ff75b5b3
Sergey Karlinskiy <s9219871110@gmail.com>
2020-04-15 16:34:07 +0300

    refactored import of constants and helpers

--- routes/error.js
+++ routes/error.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const error = require('../errors/Errors');
-const message = require('../errors/messages');
+const error = require('../helpers');
+const message = require('../constants');
 
 router.all('*', (req, res, next) => next(new error.NotFound(message.ITEM_NOT_FOUND)));
 
