dd223d28ee93a2b8bacfcbc3bbc7df03659a9b09
Sergey Karlinskiy <s9219871110@gmail.com>
2020-04-15 16:34:44 +0300

    added prehook

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -42,5 +41,15 @@ userSchema.statics.findUserByCredentials = function (email, password) {
         return user;
       }));
 };
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  return bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    return next();
+  });
+});
 
 module.exports = mongoose.model('user', userSchema);
